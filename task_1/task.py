f6105381e1add930180ca5578930dce7bb3da107
Anastasia <rotaru.nastia@gmail.com>
2023-11-26 19:07:50 +0200

    aded improvements

--- task_1/task.py
+++ task_1/task.py
@@ -1,6 +1,30 @@
-books=int(input("Здравствуйте сколько книг вы прочли?"))
-yearsBooks=int(input("За сколько лет вы прочли все эти книги?"))
-movies=int(input("А сколько фильмов вы посмотрели?"))
-yearsMovies=int(input("За сколько лет вы просмотрели все эти фильмы?"))
-print("Значит в год вы читаете ",books//yearsBooks,"книг")
-print("Значит в год вы смотрите ",movies//yearsMovies,"фильмов")
+# books #
+books =input("Здравствуйте сколько книг вы прочли?\n")
+
+while not books.isdigit():
+    print(f"Упс , вы ввели не число, а то что вы ввели это '{books}' \n")
+    books =input("Введите еще раз сколько книг вы прочли?\n")
+
+# years Books #
+yearsBooks = input("За сколько лет вы прочли все эти книги?\n")
+
+while not yearsBooks.isdigit():
+    print(f"Упс , вы ввели не число, а то что вы ввели это '{yearsBooks}' \n")
+    yearsBooks =input("Введите еще раз за сколько лет вы прочли эти книги?\n")
+
+# movies #
+movies = input("А сколько фильмов вы посмотрели?\n")
+
+while not movies.isdigit():
+    print(f"Упс , вы ввели не число, а то что вы ввели это '{movies}' \n")
+    movies =input("Введите еще раз за сколько фильмов вы посмотрели?\n")
+
+# yearsMovies #
+yearsMovies = input("За сколько лет вы просмотрели все эти фильмы?\n")
+while not yearsMovies.isdigit():
+    print(f"Упс , вы ввели не число, а то что вы ввели это '{yearsMovies}' \n")
+    yearsMovies =input("Введите еще раз за сколько лет вы просмотрели эти фильмы?\n")
+
+# outputYear #
+print("Значит в год вы читаете", int(books)//int(yearsBooks), "книг")
+print("Значит в год вы смотрите", int(movies)//int(yearsMovies), "фильмов")
