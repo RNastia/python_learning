52c2fcf03058478a84cc66dfcb43b18bf8ffed97
Anastasia <rotaru.nastia@gmail.com>
2023-12-03 21:46:50 +0200

    я добавила задачу только я и никто другой !

--- dop.tack/task_3.py
+++ dop.tack/task_3.py
@@ -1,17 +1,20 @@
 name=input("Напишите пожалуйста ваше имя ,а так же отчество : ")
 
+while name!="Илья Игоревич":
+    print("Вы ввели не ваше имя и отчество)")
+    name=input("Введите их еще раз пожалуйста :")
+
 # summa
 summa=input(f"Здравствуйте {name}, введите пожалуйста сумму которую вы хотите положить в банк : ")
 while not summa.isdigit():
     print(f"Упс , вы ввели не число, а то что вы ввели это '{summa}' \n")
     summa =input("Введите еще раз сумму которую вы хотите положить в банк\n")
 
-#time in months
+# time in months
 time=input(f"{name} а сейчас введите на какой период вы хотите сделать вклад (в месяцах): ")
 while not time.isdigit():
     print(f"Упс , вы ввели не число, а то что вы ввели это '{time}' \n")
     time =input("Введите еще раз на какой период вы хотите сделать вклад (в месяцах)?\n")
-
 
 summa=int(summa)
 procent = 5.5
@@ -23,9 +26,4 @@ for month in range(int(time)):
     print(f"В {month+1} месяце сумма: ",summaMonth)
 
 print("------------------------------")
-print("Итоговая сумма :", summaMonth)
-
-
-
-
-   
+print("Итоговая сумма :", summaMonth) 
