56304320d58c0cb0867da2c51c1a70088f241218
Anastasia <rotaru.nastia@gmail.com>
2023-12-10 14:04:29 +0200

    что хочешь

---
+++ spischi/spisok.py
@@ -0,0 +1,8 @@
+spisok1=input("Ведите пожалуйста числа через пробел : ")
+spisok2=input("Отлично теперь еще какие-то числа через пробел : ")
+array=spisok1.split()
+array2=spisok2.split()
+
+spisocek=array+array2
+print(sorted(spisocek))
+
